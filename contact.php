a1c0f470303c6027624e71655dee9cd1f99fcd4c
MuindiKelvin <kevinmuindi10@students.uonbi.ac.ke>
2022-05-24 23:36:50 +0300

    Initial commit

---
+++ contact.php
@@ -0,0 +1,69 @@
+<!-- Contact -->
+<h1>Contact Us</h1>
+  <div class="contact">
+    <div class="contact_container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="section_title text-center">
+              <div>Ciao</div>
+              <h1>Say Hello</h1>
+            </div>
+            <div class="contact_text text-center">
+              <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis.</p>
+            </div><br><br><br>
+            <div class="contact_form_container">
+              <form action="#" class="contact_form text-center">
+                <div class="col-lg-16">
+                  <div class="col-lg-16">
+                    <input type="text" class="contact_input" placeholder="Your name" required="required">
+                  </div><br><br>
+                  <div class="col-lg-16">
+                    <input type="email" class="contact_input" placeholder="Your email" required="required">
+                  </div>
+                </div><br>
+                <div class="col-lg-16">
+                  <input type="text" class="contact_input" placeholder="Subject">
+                </div><br><br>
+                
+                <div class="col-lg-16">
+                  <textarea class="contact_input" placeholder="Message" required="required"></textarea>
+                </div>
+
+                <div>
+                  <button class="contact_button">send message</button>
+                </div><br>
+                
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <div class="contact_map_container">
+      <div class="map">
+        <div id="google_map" class="google_map">
+          <div class="map_container">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Contact Map Content -->
+      <div class="contact_map_content">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+          <img class="contact_info_logo_1" src="images/logo_4.png" alt="">
+          <img class="contact_info_logo_2" src="images/logo_2.png" alt="">
+          <div class="contact_info_list">
+            <ul class="text-center">
+              <li>132 Liberty Streetelit, Plano, Texas</li>
+              <li>hello@home.com</li>
+              <li>214-805-4428</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
